82c54c3882aee603b1d2f772f8ac0ea646bde57b
Nick <vo.nick2868@gmail.com>
2021-12-08 18:40:19 -0800

    Add files via upload

---
+++ StockCheck/Controllers/StockCheckController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace StockCheck.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockCheckController : ControllerBase
+    {
+        private readonly ILogger<StockCheckController> _logger;
+
+        public StockCheckController(ILogger<StockCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<StockCheck> Get()
+        {
+            var rng = new Random();
+            IEnumerable<StockCheck> update = Enumerable.Range(1, 100).Select(index => new StockCheck
+            {
+                itemID = rng.Next(1, 5),
+                stockLeft = rng.Next(0, 55)
+            });
+            
+            return update;
+        }
+    }
+}
